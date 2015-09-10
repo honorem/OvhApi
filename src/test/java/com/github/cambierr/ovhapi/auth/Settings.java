47a3f9c53911d6e8321b5ebf7cfdea3e9f32a0bb
honorem <honorem@honorem-laptop>
2015-09-10 08:26:35 +0200

    removed cred info

--- src/test/java/com/github/cambierr/ovhapi/auth/Settings.java
+++ src/test/java/com/github/cambierr/ovhapi/auth/Settings.java
@@ -10,10 +10,10 @@ package com.github.cambierr.ovhapi.auth;
  * @author honorem
  */
 public class Settings {
-    static String applicationKey = "s5y9zFUNXIJqKIl1";
-    static String applicationSecret = "0Egnnx62AHCiRZcPkGjVfdEM3cs1FzS3";
-    static String redirection = "http://example.com";
-    static String credentialToken = "R9hkdirx1r9754OuCXOyLCfJbN0ANynLelkkI2sF6GfZkl6BoqkF4JaYf4yf50lc";
-    static String consumerKey = "TvO9PRiRI4xZ7NOMS1hC4Z9f01k9Q94o";
+    static String applicationKey = "";
+    static String applicationSecret = "";
+    static String redirection = "";
+    static String credentialToken = "";
+    static String consumerKey = "";
     static String validationUrl = "https://eu.api.ovh.com/auth/?credentialToken=";
 }

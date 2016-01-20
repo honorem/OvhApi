f135d87aaf7325845e6f088ba1b2f2369431cc3b
honorem <marc@shareif.com>
2016-01-20 14:16:39 +0100

    add header Content-type

--- src/main/java/com/github/cambierr/ovhapi/common/RequestBuilder.java
+++ src/main/java/com/github/cambierr/ovhapi/common/RequestBuilder.java
@@ -108,7 +108,7 @@ public class RequestBuilder {
                 req = ((body == null) ? Unirest.post(completePath) : Unirest.post(completePath).header("Content-type", "application/json").body(body)).getHttpRequest();
                 break;
             case PUT:
-                req = ((body == null) ? Unirest.put(completePath) : Unirest.post(completePath).header("Content-type", "application/json").body(body)).getHttpRequest();
+                req = ((body == null) ? Unirest.put(completePath) : Unirest.put(completePath).header("Content-type", "application/json").body(body)).getHttpRequest();
                 break;
             default:
                 throw new RuntimeException("wrong method received");
